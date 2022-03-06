6585a76716c06f14ebcddf691a7b395ce2b26d18
Alibaba Ibadullayev <ali.ibadullayev.02@gmail.com>
2022-03-06 23:58:31 +0300

    final_mb

--- 2nd lab/src/Main.java
+++ 2nd lab/src/Main.java
@@ -4,9 +4,39 @@ public class Main {
     public static void main(String[] args) throws IOException {
         IO io = new IO();
 
-        double[] inputData = io.readData(args[0]);
-        int function = (int) inputData[0];
-        if (function > 3 || function < 1) System.err.println("In program is no such function;");
+        double[] inputData = io.readData(args.length==0 ? "" : args[0]);
+        int num = (int) inputData[0];
+        double a = inputData[1];
+        double b = inputData[2];
+        double epsilon = inputData[3];
+        double x = (a+b)/2;
+        // count of iterations
+        int n = 0;
+        if (num > 3 || num < 1) {
+            System.err.println("In program is no such function;");
+            System.exit(-1);
+        }
+        if (!verify(num, a, b )) {
+            System.err.println("This function has no root");
+            System.exit(-1);
+        }
+        while (Math.abs(a-b)>epsilon && Math.abs(getFunc(num, x))>=epsilon){
+            x = (a+b)/2;
+            if (getFunc(num, a)*getFunc(num, x)>0 ) a = x;
+            else b = x;
+            n++;
+            System.out.println("Iteration " + n);
+            System.out.format("x -> %.3f\n" , x);
+            System.out.format("f(x) -> %.3f\n", getFunc(num, x));
+            System.out.format("|a-b| -> %.3f\n" , Math.abs(a-b));
+            System.out.println();
+        }
+        System.out.println("<<RESULT>>");
+        System.out.println("End t " + n + " iteration");
+        System.out.format("x -> %.3f\n" , x);
+        System.out.format("f(x) -> %.3f\n", getFunc(num, x));
+        System.out.format("|a-b| -> %.3f\n" , Math.abs(a-b));
+        System.out.println();
 
     }
 
@@ -19,5 +49,10 @@ public class Main {
             return Math.sin(x) - 2 * Math.pow(x, 3) + Math.pow(x, 2);
         }
         return Double.MAX_VALUE;
+    }
+
+    //true - if all is good verified, else - false
+    static boolean verify(int num, double a, double b){
+        return getFunc(num, a) * getFunc(num, b) < 0;
     }
 }
